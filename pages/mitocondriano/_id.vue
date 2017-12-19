7b245f0cba1357e9a069392208756e6382685c63
nedilio <izquierdonelson@gmail.com>
2017-12-19 15:19:22 -0300

    Leyendo datos de store en el single

--- pages/mitocondriano/_id.vue
+++ pages/mitocondriano/_id.vue
@@ -1,12 +1,12 @@
 <template>
 <div class="container" id="mitocondrianos">
     <div class="row">
-        <h2>{{integrante[0].cargo}}</h2>
+        <h2>{{empleado.cargo}}</h2>
     </div>
     <div class="row">
         <div class="col-xs-12 text-center">
-            <img v-bind:src="integrante[0].fotografia" class="img-responsive img-circle" v-bind:alt="integrante[0].nombre+' '+integrante[0].apellido">
-            <h1>{{integrante[0].nombre+' '+integrante[0].apellido}}</h1>
+            <img v-bind:src="empleado.fotografia" class="img-responsive img-circle" v-bind:alt="empleado.nombre+' '+empleado.apellido">
+            <h1>{{empleado.nombre+' '+empleado.apellido}}</h1>
         </div>
     </div>
     <div class="row">
@@ -14,7 +14,7 @@
             <p><strong>email</strong></p>
         </div>
         <div class="col-xs-9">
-            <p>{{integrante[0].email}}</p>
+            <p>{{empleado.email}}</p>
         </div>
     </div>
     <div class="row">
@@ -22,7 +22,7 @@
             <p><strong>empresa:</strong></p>
         </div>
         <div class="col-xs-9">
-            <p>{{integrante[0].empresa}}</p>
+            <p>{{empleado.empresa}}</p>
         </div>
     </div>
     <div class="row">
@@ -30,7 +30,7 @@
             <p><strong>dirección:</strong></p>
         </div>
         <div class="col-xs-9">
-            <p>{{integrante[0].direccion}}</p>
+            <p>{{empleado.direccion}}</p>
         </div>
     </div>
     <div class="row">
@@ -38,7 +38,7 @@
             <p><strong>edad:</strong></p>
         </div>
         <div class="col-xs-9">
-            <p>{{integrante[0].edad}}</p>
+            <p>{{empleado.edad}}</p>
         </div>
     </div>
     <div class="row">
@@ -46,7 +46,7 @@
             <p><strong>cargo:</strong></p>
         </div>
         <div class="col-xs-9">
-            <p>{{integrante[0].cargo}}</p>
+            <p>{{empleado.cargo}}</p>
         </div>
     </div>
     <div class="row">
@@ -54,7 +54,7 @@
             <p><strong>bio:</strong></p>
         </div>
         <div class="col-xs-9">
-            <p>{{integrante[0].bio}}</p>
+            <p>{{empleado.bio}}</p>
         </div>
     </div>
 </div>
@@ -66,71 +66,25 @@
 import axios from 'axios'
 export default {
 
-    async asyncData({ req, params }) {
-                var path = $nuxt.$route.fullPath.split('/')[1]; //esto supongo no se debería hacer
-        if (path == 'ejecutivas') {
-            // console.log('Vengo de la pagina de ejecutivas');
+    computed: {
+      empleado () {
+           var path = $nuxt.$route.fullPath.split('?')[1]
+           var i = $nuxt.$route.path.split('/')[2]
+           console.log(i)
+
+        //    console.log (path)
+        //    var i = path.split('?')[1]
+           if (path == 'ejecutiva') {
             var traer = 'ejecutivas_de_cuentas'
         } else if (path == 'frontend') {
-            // console.log('Vengo de la pagina de frontend');
             var traer = 'frontend';
-            
-        } else if (path == 'mitocondriano') {
-            // console.log('recargando la misma pagina');  
-            var recarga = $nuxt.$route.fullPath.split('?')[1]; 
-            // console.log('recargando y traer datos de '+recarga);
-            if (recarga == 'ejecutivas') {
-                // console.log('recargando la misma pagina desde ejecutivas!');
-                var traer = 'ejecutivas_de_cuentas';
-                // console.log(traer);
-            } else if (path == 'frontend') {
-                // console.log('recargando la misma pagina desde frontend!');
-                var traer = 'frontend'; 
-                // console.log(traer);
-                
-            }      
         }
-        
-        return axios.get('http://pruebas.piloto.cl/JSONs/mitocondrianos.json')
-        .then((res) => {
-            console.log(traer);
-            var integrantes = res.data[traer].integrantes;
-            console.log('estoy leyendo los datos de: '+path);
-            var integranteArray = [];
-            // return console.log(integrantes);
-            integrantes.forEach(function(item, i){
-                // console.log(item.id);
-                if(item.id == params.id){
-                    // console.log('hay alguien y es el id = '+item.id);
-                    integranteArray.push(item)
-                }
-            });
-            // console.log(params);
-            return {integrante: integranteArray}
-            
-        }).catch(function (error) {
-            console.log(error);
-        })
+        return this.$store.state.empleados[traer].integrantes[i];
+      },
+      title () {
+          return this.$store.state.empleados[traer].nombre
+      }
     },
-    data: () => ({
-        function (){
-            var title = $nuxt.$route.fullPath.split('/')[1]; //esto supongo no se debería hacer
-            console.log('lhsadfjjsdafjlsdhjgfjgjfjl');
-            // if (title == 'ejecutivas') {
-            //     return{pageTitle: 'Ejecutivas de cuentas',}
-            // } else {
-                return{pageTitle: 'Frontend',}
-                
-            // }
-        },
-                // pageTitle: 'Frontend',
-        
-       
-  }),
   
 }    
 </script>
-
-<style>
-
-</style>
